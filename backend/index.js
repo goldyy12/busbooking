87af871bfefdb42268fc668bc8431c7fdcfd23d8
Diar <diarselmani10@gmail.com>
2026-07-09 12:12:59 +0200

    health

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ app.use("/api/trips", tripRoutes);
 app.use("/api/auth", authRoutes);
 
 const server = http.createServer(app);
-
+app.get("/health", (req, res) => res.status(200).send("OK"));
 const io = new Server(server, {
   cors: {
     origin: allowedOrigins,
